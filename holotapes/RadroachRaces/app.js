bfe8e2641dc3d41847a9e5e7b8e76225bc54a1de
Theeohn テオン <theeohn@gmail.com>
2026-07-03 19:42:02 -0500

    Apply suggestion from @CodyTolene
    Co-authored-by: Cody Tolene <CodyTolene@users.noreply.github.com>

--- holotapes/RadroachRaces/app.js
+++ holotapes/RadroachRaces/app.js
@@ -344,7 +344,7 @@
     if (bounced) {
       if (getTime() - lastFlapTime >= 0.2) {
         lastFlapTime = getTime();
-        Pip.audioStart('HOLO/RADROACH_RACES/assets/FLAP.WAV');
+        Pip.audioStart('HOLO/RADROACH_RACES/FLAP.WAV');
       }
 
       const vxAbs = r.vx < 0 ? -r.vx : r.vx;
